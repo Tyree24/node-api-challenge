df7b095e82499b0b799e33336a57e2c7c7f8cd8b
Tyree Lebeau <tyreelebeau@Tyrees-iMac.local>
2020-04-07 22:28:01 -0500

    trying to fix

--- routes/ProjectsRouter.js
+++ routes/ProjectsRouter.js
@@ -2,67 +2,88 @@ const express = require('express');
 
 const Projects = require('../data/helpers/projectModel.js');
 const pMid = require("../middleware/projectsMiddleware.js");
-// const ActionsRouter = require('./ActionsRouter.js');
+const ActionsRouter = require('./ActionsRouter.js');
 
 const router = express.Router();
 router.use(express.json());
-// router.use('/:id/actions', ActionsRouter);
+router.use('/:id/actions', ActionsRouter);
 
 // create a project
-router.post('/', pMid.validateProject, (req, res) => {
-  Projects.insert(req.body)
-    .then(resp => {
-      res.status(201).json(resp);
-    })
-    .catch(err => {
-      res.status(500).json({ errorMessage: "There was an error creating a project." })
-    })
-})
+router.post(
+  '/',
+  pMid.validateProject,
+  (req, res) => {
+    Projects.insert(req.body)
+      .then(resp => {
+        res.status(201).json(resp);
+      })
+      .catch(err => {
+        res.status(500).json({ errorMessage: "There was an error creating a project." })
+      })
+  }
+)
 
 // get all projects
-router.get('/', (req, res) => {
-  Projects.get()
-    .then(resp => {
-      res.status(200).json(resp);
-    })
-    .catch(err => {
-      res.status(500).json({ errorMessage: "There was an error getting projects." })
-    })
-})
+router.get(
+  '/',
+  (req, res) => {
+    Projects.get()
+      .then(resp => {
+        res.status(200).json(resp);
+      })
+      .catch(err => {
+        res.status(500).json({ errorMessage: "There was an error getting projects." })
+      })
+  }
+)
 
 // get project by id
-router.get('/:id', pMid.validateProjectById, (req, res) => {
-  const { id } = req.params;
-  Projects.get(id)
-    .then(resp => {
-      res.status(200).json(resp);
-    })
-    .catch(err => {
-      res.status(500).json({ errorMessage: "There was an error getting project." })
-    })
-})
+router.get(
+  '/:id',
+  pMid.validateProjectById,
+  (req, res) => {
+    const { id } = req.params;
+    Projects.get(id)
+      .then(resp => {
+        res.status(200).json(resp);
+      })
+      .catch(err => {
+        res.status(500).json({ errorMessage: "There was an error getting project." })
+      })
+  }
+)
 
 // update a project
-router.put('/:id', pMid.validateProjectById, pMid.validateProject, (req, res) => {
-  const { id } = req.params;
-  Projects.update(id, req.body)
-    .then(resp => {
-      res.status(200).json(resp);
-    })
-    .catch(err => {
-      res.status(500).json({ errorMessage: "There was an error updating the project." })
-    })
-})
+router.put(
+  '/:id',
+  pMid.validateProjectById,
+  pMid.validateProject,
+  (req, res) => {
+    const { id } = req.params;
+    Projects.update(id, req.body)
+      .then(resp => {
+        res.status(200).json(resp);
+      })
+      .catch(err => {
+        res.status(500).json({ errorMessage: "There was an error updating the project." })
+      })
+  }
+)
 
 // delete a project
-router.delete('/:id', pMid.validateProjectById, (req, res) => {
-  const { id } = req.params;
-  Projects.remove(id)
-    .then(resp => {
-      res.status(201).json({ message: "Project removed." })
-    })
-    .catch(err => {
-      res.status(500).json({ errorMessage: "There was an error deleting project." })
-    })
-})
+router.delete(
+  '/:id',
+  pMid.validateProjectById,
+  (req, res) => {
+    const { id } = req.params;
+    Projects.remove(id)
+      .then(resp => {
+        res.status(201).json({ message: "Project removed." })
+      })
+      .catch(err => {
+        res.status(500).json({ errorMessage: "There was an error deleting project." })
+      })
+  }
+)
+
 module.exports = router;
